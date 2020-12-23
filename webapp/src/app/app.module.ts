aab1dd2718418a2885aae244b7157cd44d49a434
fofana23 <alfof1997@gmail.com>
2020-12-22 18:55:36 -0600

    rough

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { XxComponent } from './components/test/xx/xx.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XxComponent
+    XxComponent,
+    ProfileComponent,
+    DashboardComponent,
+    HomeComponent,
+    LogoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
